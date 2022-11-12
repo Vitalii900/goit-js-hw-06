8d5835b62a76df54a166d5c3c5f2c47c230c6e1d
Vitalii Melnyk <melnikvitaliy30@gmail.com>
2022-11-12 08:10:09 +0200

    task 5

--- js/task-05.js
+++ js/task-05.js
@@ -3,13 +3,13 @@ const inputRef = document.querySelector('#name-input');
 const nameRef = document.querySelector('#name-output');
 
 const onInputChanger = (event) => {
-    console.log(event.currentTarget.placeholder);
-    if (event.currentTarget.placeholder === 'Please enter your name') {
-        nameRef.textContent = 'Anonymous';
+    // console.log(event.currentTarget.value.length);
+    if (event.currentTarget.value.length === 0) {
+        nameRef.textContent = "Anonymous";
+        return;
     };
     
     nameRef.textContent = event.currentTarget.value;
 };
 
 inputRef.addEventListener('input', onInputChanger);
-
