56e03bac33a101cb510d832326da984ba92152f6
Vitalii Melnyk <melnikvitaliy30@gmail.com>
2022-11-12 23:15:10 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,44 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const boxesRef = document.querySelector('.boxes');
+
+const firstDivBox = document.createElement('div');
+firstDivBox.style.backgroundColor = getRandomHexColor();
+firstDivBox.style.width = '30px';
+firstDivBox.style.height = '30px';
+
+const addNewDivBoxes = [];
+
+function createBoxes(amount) {
+  if (addNewDivBoxes === []) {
+    addNewDivBoxes.push(firstDivBox);
+  };
+
+  for (let i = 0; i <= amount; i += 1) {
+    const newDiv = document.createElement('div');
+    newDiv.style.backgroundColor = getRandomHexColor();
+    newDiv.style.width = `${firstDivBox.style.width} + 10px`;
+    newDiv.style.height = `${firstDivBox.style.height} + 10px`;
+    addNewDivBoxes.push(newDiv);
+  };
+};
+
+let counter = 0;
+
+function getAmountOfDiv(event) {
+  counter = event.currentTarget.value;
+  // console.log(counter)
+}
+
+const inputRef = document.querySelector('.number');
+const buttonCreateRef = document.querySelector('[data-create]');
+const buttonDestroyRef = document.querySelector('[data-destroy]');
+
+
+buttonCreateRef.addEventListener('click', createBoxes(counter));
+
+inputRef.addEventListener('input', getAmountOfDiv);
+
+// Не вийшло в мене зробити це завдання, можливо ви мені покажете рішення щоб я розібрався!!!
