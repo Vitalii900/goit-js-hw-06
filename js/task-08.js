1854b3e363be8e26622fde0466b9f6c244e5e9ff
Vitalii Melnyk <melnikvitaliy30@gmail.com>
2022-11-12 13:44:58 +0200

    task 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const formRef = document.querySelector('.login-form');
 
+
+formRef.addEventListener('submit', fieldsCheck);
+
+function fieldsCheck(event) {
+
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (email.length === 0 || password.length === 0) {
+        alert('Заповніть всі поля');
+        return;
+    }
+
+    const formData = {
+        email,
+        password,
+    };
+
+    console.log(formData)
+
+    formRef.reset();
+};
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.body;
+
+const buttonRef = document.querySelector('.change-color');
+
+buttonRef.addEventListener('click', changeColor);
+
+function changeColor() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+};
