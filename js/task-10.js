f55c776341051ecccd4a9caba22d123a4092f292
Vitalii Melnyk <melnikvitaliy30@gmail.com>
2022-11-13 20:18:31 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -13,36 +13,28 @@ firstDivBox.style.height = '30px';
 
 const addNewDivBoxes = [];
 
-function createBoxes(amount) {
+function createBoxes() {
+  // console.log(Number(inputEl.value))
   if (addNewDivBoxes === []) {
     addNewDivBoxes.push(firstDivBox);
   };
 
-  for (let i = 0; i <= amount; i += 1) {
+  for (let i = 0; i <= Number(inputEl.value); i += 1) {
     const newDiv = document.createElement('div');
     newDiv.style.backgroundColor = getRandomHexColor();
-    newDiv.style.width = `${firstDivBox.style.width} + 10`;
-    newDiv.style.height = `${firstDivBox.style.height} + 10`;
+    newDiv.style.width = `${firstDivBox.style.width} + 10 * ${[i]}`;
+    newDiv.style.height = `${firstDivBox.style.height} + 10 * ${[i]}`;
     addNewDivBoxes.push(newDiv);
   };
 };
-
-// let counter = 0;
-
-// function getAmountOfDiv(event) {
-//   counter = Number(event.currentTarget.value);
-//   // console.log(counter)
-// }
 
 const inputRef = document.querySelector('.number');
 const buttonCreateRef = document.querySelector('[data-create]');
 const buttonDestroyRef = document.querySelector('[data-destroy]');
 
 
-buttonCreateRef.addEventListener('click', createBoxes(inputEl.value));
+buttonCreateRef.addEventListener('click', createBoxes);
 
 boxesRef.append(...addNewDivBoxes);
-
-// inputRef.addEventListener('input', getAmountOfDiv);
 
 // Не вийшло в мене зробити це завдання, можливо ви мені покажете рішення щоб я розібрався!!!
